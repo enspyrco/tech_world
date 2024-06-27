e3a5c7d811eb4c8b4db49bf8868a8fa493c95b0a
Nick Meinhold <nick@enspyr.co>
2024-06-27 16:16:14 +1000

    Change player to SpriteAnimationGroupComponent

--- lib/flame/player.dart
+++ lib/flame/player.dart
@@ -1,118 +1,154 @@
+import 'dart:async';
+
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flutter/services.dart';
 import 'package:tech_world/flame/tech_world_game.dart';
 
 enum Direction { up, down, left, right }
 
-class PlayerComponent extends PositionComponent
+class PlayerComponent extends SpriteAnimationGroupComponent<Direction>
     with KeyboardHandler, HasGameReference<TechWorldGame> {
-  PlayerComponent({required super.position})
-      : animation = {
-          Direction.up: _PlayerUpAnimation(position: position),
-          Direction.down: _PlayerDownAnimation(position: position),
-          Direction.left: _PlayerLeftAnimation(position: position),
-          Direction.right: _PlayerRightAnimation(position: position),
-        };
+  @override
+  FutureOr<void> onLoad() {
+    animations = {
+      Direction.up: SpriteAnimation.fromFrameData(
+        game.images.fromCache('NPC11.png'),
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          textureSize: Vector2(32, 64),
+          stepTime: 0.12,
+          texturePosition: Vector2(192, 0),
+        ),
+      ),
+      Direction.down: SpriteAnimation.fromFrameData(
+        game.images.fromCache('NPC11.png'),
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          textureSize: Vector2(32, 64),
+          stepTime: 0.12,
+        ),
+      ),
+      Direction.left: SpriteAnimation.fromFrameData(
+        game.images.fromCache('NPC11.png'),
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          textureSize: Vector2(32, 64),
+          stepTime: 0.12,
+          texturePosition: Vector2(96, 0),
+        ),
+      ),
+      Direction.right: SpriteAnimation.fromFrameData(
+        game.images.fromCache('NPC11.png'),
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          textureSize: Vector2(32, 64),
+          stepTime: 0.12,
+          texturePosition: Vector2(288, 0),
+        ),
+      ),
+    };
+    return super.onLoad();
+  }
 
-  SpriteAnimationComponent? currentAnimation;
-  final Map<Direction, SpriteAnimationComponent> animation;
-
-  void startMoving(Direction direction) {
-    if (currentAnimation != null) game.remove(currentAnimation!);
-    currentAnimation = animation[direction];
-    game.add(currentAnimation!);
+  PlayerComponent({required super.position}) {
+    current = Direction.up;
   }
 
   @override
   bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
     if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
-      startMoving(Direction.left);
+      current = Direction.left;
+      add(MoveByEffect(Vector2(-64, 0), EffectController(duration: 0.5)));
     } else if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
-      startMoving(Direction.right);
+      current = Direction.right;
+      add(MoveByEffect(Vector2(64, 0), EffectController(duration: 0.5)));
     } else if (keysPressed.contains(LogicalKeyboardKey.arrowUp)) {
-      startMoving(Direction.up);
+      current = Direction.up;
+      add(MoveByEffect(Vector2(0, -64), EffectController(duration: 0.5)));
     } else if (keysPressed.contains(LogicalKeyboardKey.arrowDown)) {
-      startMoving(Direction.down);
+      current = Direction.down;
+      add(MoveByEffect(Vector2(0, 64), EffectController(duration: 0.5)));
     }
     return true;
   }
 }
 
-class _PlayerDownAnimation extends SpriteAnimationComponent
-    with HasGameReference<TechWorldGame> {
-  _PlayerDownAnimation({
-    required super.position,
-  }) : super(size: Vector2(32, 64), anchor: Anchor.center);
+// class _PlayerDownAnimation extends SpriteAnimationComponent
+//     with HasGameReference<TechWorldGame> {
+//   _PlayerDownAnimation({
+//     required super.position,
+//   }) : super(size: Vector2(32, 64), anchor: Anchor.center);
 
-  @override
-  void onLoad() {
-    animation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('NPC11.png'),
-      SpriteAnimationData.sequenced(
-        amount: 3,
-        textureSize: Vector2(32, 64),
-        stepTime: 0.12,
-      ),
-    );
-  }
-}
+//   @override
+//   void onLoad() {
+//     animation = SpriteAnimation.fromFrameData(
+//       game.images.fromCache('NPC11.png'),
+//       SpriteAnimationData.sequenced(
+//         amount: 3,
+//         textureSize: Vector2(32, 64),
+//         stepTime: 0.12,
+//       ),
+//     );
+//   }
+// }
 
-class _PlayerLeftAnimation extends SpriteAnimationComponent
-    with HasGameReference<TechWorldGame> {
-  _PlayerLeftAnimation({
-    required super.position,
-  }) : super(size: Vector2(32, 64), anchor: Anchor.center);
+// class _PlayerLeftAnimation extends SpriteAnimationComponent
+//     with HasGameReference<TechWorldGame> {
+//   _PlayerLeftAnimation({
+//     required super.position,
+//   }) : super(size: Vector2(32, 64), anchor: Anchor.center);
 
-  @override
-  void onLoad() {
-    animation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('NPC11.png'),
-      SpriteAnimationData.sequenced(
-        amount: 3,
-        textureSize: Vector2(32, 64),
-        stepTime: 0.12,
-        texturePosition: Vector2(96, 0),
-      ),
-    );
-  }
-}
+//   @override
+//   void onLoad() {
+//     animation = SpriteAnimation.fromFrameData(
+//       game.images.fromCache('NPC11.png'),
+//       SpriteAnimationData.sequenced(
+//         amount: 3,
+//         textureSize: Vector2(32, 64),
+//         stepTime: 0.12,
+//         texturePosition: Vector2(96, 0),
+//       ),
+//     );
+//   }
+// }
 
-class _PlayerUpAnimation extends SpriteAnimationComponent
-    with HasGameReference<TechWorldGame> {
-  _PlayerUpAnimation({
-    required super.position,
-  }) : super(size: Vector2(32, 64), anchor: Anchor.center);
+// class _PlayerUpAnimation extends SpriteAnimationComponent
+//     with HasGameReference<TechWorldGame> {
+//   _PlayerUpAnimation({
+//     required super.position,
+//   }) : super(size: Vector2(32, 64), anchor: Anchor.center);
 
-  @override
-  void onLoad() {
-    animation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('NPC11.png'),
-      SpriteAnimationData.sequenced(
-        amount: 3,
-        textureSize: Vector2(32, 64),
-        stepTime: 0.12,
-        texturePosition: Vector2(192, 0),
-      ),
-    );
-  }
-}
+//   @override
+//   void onLoad() {
+//     animation = SpriteAnimation.fromFrameData(
+//       game.images.fromCache('NPC11.png'),
+//       SpriteAnimationData.sequenced(
+//         amount: 3,
+//         textureSize: Vector2(32, 64),
+//         stepTime: 0.12,
+//         texturePosition: Vector2(192, 0),
+//       ),
+//     );
+//   }
+// }
 
-class _PlayerRightAnimation extends SpriteAnimationComponent
-    with HasGameReference<TechWorldGame> {
-  _PlayerRightAnimation({
-    required super.position,
-  }) : super(size: Vector2(32, 64), anchor: Anchor.center);
+// class _PlayerRightAnimation extends SpriteAnimationComponent
+//     with HasGameReference<TechWorldGame> {
+//   _PlayerRightAnimation({
+//     required super.position,
+//   }) : super(size: Vector2(32, 64), anchor: Anchor.center);
 
-  @override
-  void onLoad() {
-    animation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('NPC11.png'),
-      SpriteAnimationData.sequenced(
-        amount: 3,
-        textureSize: Vector2(32, 64),
-        stepTime: 0.12,
-        texturePosition: Vector2(288, 0),
-      ),
-    );
-  }
-}
+//   @override
+//   void onLoad() {
+//     animation = SpriteAnimation.fromFrameData(
+//       game.images.fromCache('NPC11.png'),
+//       SpriteAnimationData.sequenced(
+//         amount: 3,
+//         textureSize: Vector2(32, 64),
+//         stepTime: 0.12,
+//         texturePosition: Vector2(288, 0),
+//       ),
+//     );
+//   }
+// }
