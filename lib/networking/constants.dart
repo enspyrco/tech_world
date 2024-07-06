d9226b22181f81fe428d89f8370b9ceeba5c1e36
Nick Meinhold <nick@enspyr.co>
2024-07-06 10:30:53 +1000

    Add constants for the server urls

---
+++ lib/networking/constants.dart
@@ -0,0 +1,4 @@
+// servers
+const australiaSoutheast1ServerUrl = '<your_server_url>';
+const usCentral1ServerUrl = '<your_server_url>';
+// eg. 'wss://websocket-server-...', 'wss://ws-game-server-...-uc.a.run.app'
