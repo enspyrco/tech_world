4172697ca825c24c61f34be48374c9302290ac4f
Nick Meinhold <nick@enspyr.co>
2024-07-06 10:22:49 +1000

    Create a PlayersService
    That has relationships with the AuthService, NetworkingService, and the
    World object, tieing everything together.

---
+++ lib/flame/players_service.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:tech_world/flame/components/player_component.dart';
+import 'package:tech_world/flame/shared/player_path.dart';
+import 'package:tech_world_networking_types/tech_world_networking_types.dart';
+
+class PlayersService {
+  PlayersService(); // {required AuthUser authUser} : _userPlayer = PlayerComponent.from(authUser);
+  final Map<String, PlayerComponent> _otherPlayersMap = {};
+  // PlayerComponent? _userPlayer;
+  StreamController<NetworkUser> userAddedController =
+      StreamController<NetworkUser>();
+  StreamController<String> userRemovedController = StreamController<String>();
+  StreamController<PlayerPath> playerPathController =
+      StreamController<PlayerPath>();
+
+  Stream<NetworkUser> get userAdded => userAddedController.stream;
+  Stream<String> get userRemoved => userRemovedController.stream;
+  Stream<PlayerPath> get playerPaths => playerPathController.stream;
+
+  void inspectAndUpdate(Set<NetworkUser> networkUsers) {
+    // add new players
+    for (final networkUser in networkUsers) {
+      if (!_otherPlayersMap.containsKey(networkUser.id)) {
+        _otherPlayersMap[networkUser.id] = PlayerComponent.from(networkUser);
+        userAddedController.add(networkUser);
+      }
+    }
+    // remove departed players
+    for (final playerId in _otherPlayersMap.keys) {
+      if (networkUsers.where((authUser) => authUser.id == playerId).isEmpty) {
+        userRemovedController.add(playerId);
+        _otherPlayersMap.remove(playerId);
+      }
+    }
+  }
+
+  void addPathToPlayer(String playerId, List<Double2> points) {
+    playerPathController.add(
+      PlayerPath(
+        playerId: playerId,
+        largeGridPoints:
+            points.map<Vector2>((point) => Vector2(point.x, point.y)).toList(),
+      ),
+    );
+  }
+}
