2c9a4a8934b6ab26ce0ae2a7b1fca5ffd79895e5
Nick Meinhold <nick@enspyr.co>
2025-10-09 02:18:51 +1100

    Use custom domain for WSL

--- lib/networking/constants.dart
+++ lib/networking/constants.dart
@@ -4,6 +4,6 @@ const usCentral1CloudRunUrl =
     'wss://tech-world-game-server-service-del2qngvxa-uc.a.run.app';
 
 const devComputeEngineUrl = 'ws://35.223.250.225:8080';
-const secureComputeEngineUrl = 'wss://35.223.250.225:8080';
+const secureComputeEngineUrl = 'wss://adventures-in-tech.world:8080';
 
 const localServerUrl = 'ws://127.0.0.1:8080';
