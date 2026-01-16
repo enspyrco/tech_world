77aaa567bf9dc01853b1ab47d63415eac66c17e1
Nick Meinhold <nick@enspyr.co>
2026-01-17 07:45:07 +1100

    Simplify FinalPage by removing ProximityVideoOverlay
    - Remove overlay system in favor of Flame-based bubbles
    - Show game full-screen with controls at bottom
    - Remove unused ProximityService dependency

--- lib/livekit/pages/final_page.dart
+++ lib/livekit/pages/final_page.dart
@@ -1,11 +1,8 @@
 import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:livekit_client/livekit_client.dart';
-import 'package:tech_world/flame/tech_world.dart';
 import 'package:tech_world/flame/tech_world_game.dart';
-import 'package:tech_world/livekit/pages/room.dart';
-import 'package:tech_world/livekit/widgets/proximity_video_overlay.dart';
-import 'package:tech_world/proximity/proximity_service.dart';
+import 'package:tech_world/livekit/widgets/controls.dart';
 import 'package:tech_world/utils/locator.dart';
 
 class FinalPage extends StatefulWidget {
@@ -19,41 +16,18 @@ class FinalPage extends StatefulWidget {
 }
 
 class _FinalPageState extends State<FinalPage> {
-  late final TechWorld _techWorld;
-  late final TechWorldGame _game;
-  late final ProximityService _proximityService;
-
-  @override
-  void initState() {
-    super.initState();
-    _techWorld = locate<TechWorld>();
-    _game = TechWorldGame(world: _techWorld);
-    _proximityService = ProximityService(proximityThreshold: 3);
-  }
-
-  @override
-  void dispose() {
-    _proximityService.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: 120, child: RoomPage(widget.room, widget.listener)),
         Expanded(
-          child: Stack(
-            children: [
-              GameWidget(game: _game),
-              ProximityVideoOverlay(
-                room: widget.room,
-                techWorld: _techWorld,
-                proximityService: _proximityService,
-              ),
-            ],
-          ),
+          child: GameWidget(game: locate<TechWorldGame>()),
         ),
+        if (widget.room.localParticipant != null)
+          SafeArea(
+            top: false,
+            child: ControlsWidget(widget.room, widget.room.localParticipant!),
+          ),
       ],
     );
   }
