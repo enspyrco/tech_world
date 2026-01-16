7545e42414235817169a8bfaea741ee91303f6a4
Nick Meinhold <nick@enspyr.co>
2026-01-17 09:24:58 +1100

    Add dark theme splash screens for iOS, Android, and macOS

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -8,6 +8,9 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    // Set Tech World dark theme background color (#1a1a2e)
+    self.backgroundColor = NSColor(red: 0.102, green: 0.102, blue: 0.180, alpha: 1.0)
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
